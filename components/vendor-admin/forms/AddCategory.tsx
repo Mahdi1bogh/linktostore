2e989af6b7e390872c8b8c79bb143eddd2bf9c3d
Mahdi1bogh <57180327+Mahdi1bogh@users.noreply.github.com>
2024-07-13 18:25:15 +0100

    category fix

--- components/vendor-admin/forms/AddCategory.tsx
+++ components/vendor-admin/forms/AddCategory.tsx
@@ -103,11 +103,11 @@ const AddCategory = () => {
                   {...register(`cats.${index}.name`)}
                   className="mt-1 block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
-                {errors.cats && errors.cats[index] && (
+                {/* {errors.cats && errors.cats[index] && (
                   <p className="text-red-500 text-sm ml-1 mt-1">
                     {errors.cats[index].name?.message}
                   </p>
-                )}
+                )} */}
               </div>
               {index > 0 && (
                 <Button
